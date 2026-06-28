1d0ad17018dcd741b519d92760c02fa60397a804
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-06-28 06:05:31 +0000

    Add Medium solution for 4348: Maximum Total Sum of K Selected Elements

---
+++ leetcode/medium/4348-maximum-total-sum-of-k-selected-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    using ll = long long;
+public:
+    long long maxSum(vector<int>& nums, int k, int mul) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        long long sum = 0;
+        for (int i = n - 1; i >= n - k; i--) {
+            if (mul > 0) {
+                sum += 1LL * nums[i] * mul;
+                mul--;
+            }
+            else sum += nums[i];
+        }
+        return sum;
+    }
+};
