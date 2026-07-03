2ce836cbea15ab08d021f2227b6903440dfbcc82
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-07-03 12:35:01 +0000

    Add Medium solution for 63: Unique Paths II

---
+++ leetcode/medium/63-unique-paths-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& g) {
+        int n = g.size(), m = g[0].size();
+        vector<vector<int>> dp(n, vector<int> (m, 0));
+        if (g[0][0] == 1) return 0;
+        bool flag = true;
+        for (int i = 0; i < n; i++) {
+            if (g[i][0] == 1) flag = false;
+            if (!flag) break;
+            dp[i][0] = 1;
+        }
+        flag = true;
+        for (int i = 0; i < m; i++) {
+            if (g[0][i] == 1) flag = false;
+            if (!flag) break;
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (g[i][j] != 1) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
