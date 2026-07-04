4572f0b4f10f11a9991b79818843a31527592971
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-07-04 15:34:35 +0000

    Add Medium solution for 4334: Minimum Operations to Transform Binary String

---
+++ leetcode/medium/4334-minimum-operations-to-transform-binary-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    const int INT = 1e6;
+public:
+    int minOperations(string s1, string s2) {
+        /*
+            s1 = 110
+            s2 = 101
+            greedy fails ig;
+            because 110 -> 000 -> 100 -> 101 3 steps
+            can't just iterate 
+            dp state i and curr
+            actually we can use greedy
+            first convert all 0 to 1 in s1 if 1 in s2 then 
+            for 2nd case when s1 = 1 and s2 = 0 jsut pair, then even odd thing
+        */
+        int n = s1.size();
+        int ans = 0;
+        if (n == 1) {
+            if (s1 == s2) return 0;
+            else if (s1[0] == '0' && s2[0] == '1') return 1;
+            else return -1;
+        }
+        for (int i = 0; i < n; i++) if (s1[i] == '0' && s2[i] == '1') ans++;
+        int i = 0;
+        while (i < n) {
+            if (s1[i] == '1' && s2[i] == '0') {
+                int j = i;
+                while (j < n && s1[j] == '1' && s2[j] == '0') j++;
+                ans += (j - i) / 2;
+                if ((j - i) % 2) ans += 2;
+                i = j;
+            }
+            else i++;
+        }
+        return ans;
+    }
+};
