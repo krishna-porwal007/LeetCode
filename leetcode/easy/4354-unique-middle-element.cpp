e76789309a5e14f646cb7300d2609421e1fba56a
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-07-04 14:36:43 +0000

    Add Easy solution for 4354: Unique Middle Element

---
+++ leetcode/easy/4354-unique-middle-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMiddleElementUnique(vector<int>& nums) {
+        int n = nums.size();
+        int mid = nums[n / 2];
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == mid) cnt++;
+        }
+        if (cnt == 1) return true;
+        else return false;
+    }
+};
