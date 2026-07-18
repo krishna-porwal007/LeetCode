b185fff7cdf3008bbfea6822914a8c398f16c107
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-07-18 17:12:17 +0000

    Add Easy solution for 2106: Find Greatest Common Divisor of Array

---
+++ leetcode/easy/2106-find-greatest-common-divisor-of-array.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        int mx = *max_element(nums.begin(), nums.end()), mn = *min_element(nums.begin(), nums.end());
+        return __gcd(mx, mn);
+    }
+};
