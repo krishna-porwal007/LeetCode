c995f4c496792a641e5341a8ca6182bff958518a
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-08-04 21:39:25 +0000

    Add Medium solution for 309: Best Time to Buy and Sell Stock with Cooldown

---
+++ leetcode/medium/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int n = p.size();
+        vector<vector<int>> dp(n + 2, vector<int> (2, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= 1; j++) {
+                if (j == 0) {
+                    int pick = -p[i] + dp[i + 1][1];
+                    int np = 0 + dp[i + 1][0];
+                    dp[i][0] = max(pick, np);
+                }
+                else {
+                    int pick = p[i] + dp[i + 2][0];
+                    int np = 0 + dp[i + 1][1];
+                    dp[i][1] = max(pick, np);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
