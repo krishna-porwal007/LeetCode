83068d6e81ddb6928169d9438d854d8d06a551be
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-08-21 06:43:46 +0000

    Add Medium solution for 300: Longest Increasing Subsequence

---
+++ leetcode/medium/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= -1; j--) {
+                int notake = dp[i + 1][j + 1];
+                int take = -1;
+                if (j == -1 || nums[i] > nums[j]) take = 1 + dp[i + 1][i + 1];
+                dp[i][j + 1] = max(take, notake); 
+            }
+        }
+        return dp[0][0];
+    }
+};
