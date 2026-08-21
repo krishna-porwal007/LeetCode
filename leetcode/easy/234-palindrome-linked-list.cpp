5f6030fcf8048f80a905659f6065af9dfcad52cb
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-08-21 10:15:39 +0000

    Add Easy solution for 234: Palindrome Linked List

---
+++ leetcode/easy/234-palindrome-linked-list.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *prev = nullptr;
+        while (fast != nullptr && fast -> next != nullptr) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        if (fast != nullptr) slow = slow -> next;
+        while (slow != nullptr) {
+            ListNode *temp = slow -> next;
+            slow -> next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        ListNode *left = head;
+        ListNode *right = prev;
+        while (right != nullptr) {
+            if (left -> val != right -> val) return false;
+            left = left -> next;
+            right = right -> next;
+        }
+        return true;
+    }
+};
