cf25ff76a78eee15ba950488fe7d40d9361c6fc3
Krishna Porwal <krishna.porwal.me@gmail.com>
2026-09-04 00:01:54 +0000

    Add Easy solution for 4284: Smallest Stable Index I

---
+++ leetcode/easy/4284-smallest-stable-index-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int a = *max_element(nums.begin(), nums.begin() + i + 1);
+            int b = *min_element(nums.begin() + i, nums.end());
+            if (a - b <= k) return i;
+        }
+        return -1;
+    }
+};
